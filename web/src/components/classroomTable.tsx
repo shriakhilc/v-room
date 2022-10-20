085821d66c7df03fbbdabbe091f5601f39c6a6a3
Quinn Eggleston <qdeggles@vt.edu>
2022-10-20 12:13:49 -0400

    Add table to classroom list page

---
+++ web/src/components/classroomTable.tsx
@@ -0,0 +1,84 @@
+import { Classroom } from "@prisma/client";
+import { ScriptProps } from "next/script";
+import React from "react";
+  
+interface ClassroomTableProps {
+    classrooms: Classroom[];
+}
+
+class ClassroomTable extends React.Component<ClassroomTableProps, {}> {
+    render() {
+        return (
+            <div className="flex flex-col">
+                <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div className="py-2 align-middle min-w-full sm:px-6 lg:px-8">
+                        <div className="shadow border-b border-gray-200 sm:rounded-lg">
+                            <table className="min-w-full max-h-full divide-y divide-gray-200">
+                                <thead className="bg-gray-50">
+                                    <tr>
+                                        <th
+                                            scope="col"
+                                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                        >
+                                            Name (TBD)
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                        >
+                                            Status
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                        >
+                                            Invite Code
+                                        </th>
+                                        <th scope="col" className="relative px-6 py-3">
+                                            <span className="sr-only">Delete</span>
+                                        </th>
+                                        <th scope="col" className="relative px-6 py-3">
+                                            <span className="sr-only">Archive</span>
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody className="bg-white divide-y divide-gray-200">
+                                    {this.props.classrooms.map(classroom => (
+                                    <tr key={classroom.id}>
+                                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                                            NOT IMPLEMENTED
+                                        </td>
+                                        <td className="px-6 py-4 whitespace-nowrap">
+                                            <span
+                                                className={`px-2 inline-flex text-xs leading-5
+                                            font-semibold rounded-full ${classroom.active ? "bg-green-100 text-green-800" : "bg-red-100 text-red-800"}`}
+                                            >
+                                                {classroom.active ? 'Active' : 'Inactive'}
+                                            </span>
+                                        </td>
+                                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                            {classroom.inviteCode}
+                                        </td>
+                                        <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                            <a href="#" className="text-red-600 hover:text-red-900">
+                                                Delete
+                                            </a>
+                                        </td>
+                                        <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                            <a href="#" className="text-red-600 hover:text-red-900">
+                                                Archive
+                                            </a>
+                                        </td>
+                                    </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default ClassroomTable;
