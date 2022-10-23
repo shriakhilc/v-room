4a79f639068a432944c1e38d512dd1f213a61274
Quinn Eggleston <qdeggles@vt.edu>
2022-10-23 14:01:16 -0400

    update classroom pages to check for authentication

--- web/src/pages/classroom/[classroomId].tsx
+++ web/src/pages/classroom/[classroomId].tsx
@@ -5,10 +5,12 @@ import Footer from "../../components/footer"
 import type { Classroom, User } from '@prisma/client';
 import React from "react";
 import UserTable from "@/src/components/userTable";
-import { getAllUsers } from "../api/users";
+import { getAllUsers } from "../api/user";
 import { useRouter } from 'next/router';
 import { getClassroom } from "../api/classrooms/[classroomId]";
-import { getUsersForClassroom } from "../api/users/classrooms/[classroomId]";
+import { getUsersForClassroom } from "../api/user/classrooms/[classroomId]";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
 
 type PageProps = {
   allUsersSectioned: User[],
@@ -18,9 +20,11 @@ type PageProps = {
 
 const ClassroomDetail: NextPage<PageProps> = ({ allUsersSectioned, userRoles, classroom }) => {
   const router = useRouter();
-
+  const { data, status } = useSession();
+  console.log(status);
   return (
     <>
+
       <div className="container mx-auto">
         <Head>
           <title>V-Room</title>
@@ -29,15 +33,19 @@ const ClassroomDetail: NextPage<PageProps> = ({ allUsersSectioned, userRoles, cl
         </Head>
 
         <Header></Header>
-
-        <main className="container mx-auto h-5/6 flex flex-col items-left p-4">
-          <h1 className="text-lg leading-normal p-4">
-            <span className="text-red-500">Users in {classroom.name}</span>
-          </h1>
-          <UserTable router={router} users={allUsersSectioned} userRoles={userRoles} classroom={classroom}></UserTable>
-        </main>
-
-
+        {status == "authenticated" && (
+          <main className="container mx-auto h-5/6 flex flex-col items-left p-4">
+            <h1 className="text-lg leading-normal p-4">
+              <span className="text-red-500">Users in {classroom.name}</span>
+            </h1>
+            <UserTable router={router} users={allUsersSectioned} userRoles={userRoles} classroom={classroom}></UserTable>
+          </main>)
+        }
+        {status != "authenticated" && 
+          <main className="max-h-[50rem] min-h-[50rem]">
+            It seems you aren't logged in. Please return to <Link href={'/'}><a className="text-red-500 hover:text-decoration-underline">the home page</a></Link> to sign in, then try again.
+          </main>
+        }
         <Footer></Footer>
       </div>
     </>
--- web/src/pages/classroom/list.tsx
+++ web/src/pages/classroom/list.tsx
@@ -1,4 +1,4 @@
-import type { NextPage } from "next";
+import type { GetServerSidePropsContext, NextPage } from "next";
 import Head from "next/head";
 import Header from "../../components/header"
 import Footer from "../../components/footer"
@@ -6,20 +6,28 @@ import { getAllClassrooms } from "../api/classrooms";
 import ClassroomTable from "@/src/components/classroomTable";
 import type { Classroom } from '@prisma/client';
 import React from "react";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+import { authOptions } from "../api/auth/[...nextauth]";
+import { unstable_getServerSession } from "next-auth";
+import { getUserByEmail } from "../api/user/[userId]";
+import { getClassroomsForUser } from "../api/classrooms/users/[userId]";
 
 type PageProps = {
-  data: Classroom[];
+  classroomData: Classroom[],
+  userRoles: string[]
 }
 
-const ClassroomList: NextPage<PageProps> = ({ data }) => {
-  const [classrooms, setClassrooms] = React.useState(data);
+const ClassroomList: NextPage<PageProps> = ({ classroomData, userRoles }) => {
+  const [classrooms, setClassrooms] = React.useState(classroomData);
   const [show, setShow]  = React.useState(false);
   const [newName, setNewName]  = React.useState("");
   const [newDept, setNewDept]  = React.useState("");
   const [newCourseNumber, setNewCourseNumber]  = React.useState("");
   const [newCrn, setNewCrn]  = React.useState("");
   const [error, setError] = React.useState(false);
-  
+  const [roles, setRoles] = React.useState(userRoles);
+  const {data, status} = useSession();
 
   async function addClassroom() {
     console.log("adding classroom");
@@ -67,11 +75,12 @@ function formCompleted() {
 
         <Header></Header>
 
+        {status == "authenticated" &&
         <main className="container mx-auto h-5/6 flex flex-col items-left p-4">
           <h1 className="text-lg leading-normal p-4">
             <span className="text-red-500">Your Classrooms</span>
           </h1>
-          <ClassroomTable classrooms={classrooms}></ClassroomTable>
+          <ClassroomTable classrooms={classrooms} roles={roles}></ClassroomTable>
           <button onClick={showModal} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 m-4 rounded">Add Classroom</button>
           { show && 
             <div
@@ -82,56 +91,85 @@ function formCompleted() {
             }}
             >
 
-            <div className="relative"></div>
-              <div className="relative bg-white rounded-lg shadow">
-                <div className="flex justify-between items-start p-4 rounded-t border-b border-gray-600">
-                    <h3 className="text-xl font-semibold text-gray-900">
-                        Terms of Service
-                    </h3>
-                </div>
-                <div className="p-6 space-y-6">
-                    <p className="text-base leading-relaxed text-gray-900">
-                        Input course information.
-                    </p>
-                </div>
-                <div className="pr-6 pl-6 pb-6">
-                  <label className="block mb-2 text-sm font-medium text-gray-900">Course Name</label>
-                  <input value={newName} onChange={e => setNewName(e.target.value)} type="text" name="courseName" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5" placeholder="Intro to CS"></input>
-                </div>
-                <div className="pr-6 pl-6 pb-6">
-                  <label className="block mb-2 text-sm font-medium text-gray-900">Course Department</label>
-                  <input value={newDept} onChange={e => setNewDept(e.target.value)} type="text" name="department" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5" placeholder="CS"></input>
-                </div>
-                <div className="pr-6 pl-6 pb-6">
-                  <label className="block mb-2 text-sm font-medium text-gray-900">Course Number</label>
-                  <input value={newCourseNumber} onChange={e => setNewCourseNumber(e.target.value.replace(/\D/,''))} type="text" name="courseNumber" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5" placeholder="1001"></input>
-                </div>
-                <div className="pr-6 pl-6 pb-6">
-                  <label className="block mb-2 text-sm font-medium text-gray-900">Course CRN</label>
-                  <input value={newCrn} onChange={e => setNewCrn(e.target.value.replace(/\D/,''))} type="text" name="crn" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5" placeholder="12345"></input>
-                </div>
-                <div className="flex items-center p-6 space-x-2 rounded-b border-t border-gray-600">
-                    <button disabled={formCompleted()} onClick={() => {addClassroom()}} type="button" className="focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center disabled:bg-grey-200 disabled:text-grey:600 bg-red-500 hover:bg-red-700 text-white focus:ring-red-300">Create Classroom</button>
-                    <button onClick={() => {setShow(false); setError(false)}} type="button" className="focus:ring-4 focus:outline-none rounded-lg border text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 bg-gray-700 text-gray-300 border-gray-500 hover:text-white hover:bg-gray-600 focus:ring-gray-600">Close</button>
-                </div>
-                {error &&
-                <div>
-                  Error creating classroom.
-                </div>}
+          <div className="relative"></div>
+            <div className="relative bg-white rounded-lg shadow">
+              <div className="flex justify-between items-start p-4 rounded-t border-b border-gray-600">
+                  <h3 className="text-xl font-semibold text-gray-900">
+                      Terms of Service
+                  </h3>
               </div>
-            </div> }
+              <div className="p-6 space-y-6">
+                  <p className="text-base leading-relaxed text-gray-900">
+                      Input course information.
+                  </p>
+              </div>
+              <div className="pr-6 pl-6 pb-6">
+                <label className="block mb-2 text-sm font-medium text-gray-900">Course Name</label>
+                <input value={newName} onChange={e => setNewName(e.target.value)} type="text" name="courseName" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5" placeholder="Intro to CS"></input>
+              </div>
+              <div className="pr-6 pl-6 pb-6">
+                <label className="block mb-2 text-sm font-medium text-gray-900">Course Department</label>
+                <input value={newDept} onChange={e => setNewDept(e.target.value)} type="text" name="department" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5" placeholder="CS"></input>
+              </div>
+              <div className="pr-6 pl-6 pb-6">
+                <label className="block mb-2 text-sm font-medium text-gray-900">Course Number</label>
+                <input value={newCourseNumber} onChange={e => setNewCourseNumber(e.target.value.replace(/\D/,''))} type="text" name="courseNumber" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5" placeholder="1001"></input>
+              </div>
+              <div className="pr-6 pl-6 pb-6">
+                <label className="block mb-2 text-sm font-medium text-gray-900">Course CRN</label>
+                <input value={newCrn} onChange={e => setNewCrn(e.target.value.replace(/\D/,''))} type="text" name="crn" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5" placeholder="12345"></input>
+              </div>
+              <div className="flex items-center p-6 space-x-2 rounded-b border-t border-gray-600">
+                  <button disabled={formCompleted()} onClick={() => {addClassroom()}} type="button" className="focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center disabled:bg-grey-200 disabled:text-grey:600 bg-red-500 hover:bg-red-700 text-white focus:ring-red-300">Create Classroom</button>
+                  <button onClick={() => {setShow(false); setError(false)}} type="button" className="focus:ring-4 focus:outline-none rounded-lg border text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 bg-gray-700 text-gray-300 border-gray-500 hover:text-white hover:bg-gray-600 focus:ring-gray-600">Close</button>
+              </div>
+              {error &&
+              <div>
+                Error creating classroom.
+              </div>}
+            </div>
+          </div> }
+        </main> }
+
+        {status != "authenticated" && 
+        <main className="max-h-[50rem] min-h-[50rem]">
+          It seems you aren't logged in. Please return to <Link href={'/'}><a className="text-red-500 hover:text-decoration-underline">the home page</a></Link> to sign in, then try again.
         </main>
-
-
+        }
         <Footer></Footer>
       </div>
     </>
   );
 };
 
-export async function getServerSideProps() {
-  const data = await getAllClassrooms();
-  return { props: { data }};
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+
+  const session = await unstable_getServerSession(
+    context.req,
+    context.res,
+    authOptions);
+  if(session) {
+    const user = await getUserByEmail(session.user?.email);
+    if(user) {
+      const classrooms = await getClassroomsForUser(user.id);
+      const userRoles:string[] = [];
+      classrooms.studentClassrooms.forEach((element) => {
+        userRoles.push("Student");
+      });
+      classrooms.assistantClassrooms.forEach((element) => {
+        userRoles.push("Assistant");
+      });
+      classrooms.assistantClassrooms.forEach((element) => {
+        userRoles.push("Instructor");
+      });
+      const concatenatedClassrooms = classrooms.studentClassrooms.concat(classrooms.assistantClassrooms.concat(classrooms.instructorClassrooms));
+      return { props: { classroomData: concatenatedClassrooms, userRoles: userRoles }};
+    }
+  }
+  else {
+    return {props: {classroomData: [], userRoles: []}};
+  }
+
 }
 
 export default ClassroomList;
