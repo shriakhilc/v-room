e1e9edfe9b8ea570a993406e5ded84b76fe92faf
Quinn Eggleston <qdeggles@vt.edu>
2022-11-03 18:30:00 -0400

    Add questionRouter to trpc index

--- web/src/server/router/index.ts
+++ web/src/server/router/index.ts
@@ -3,11 +3,13 @@ import { createRouter } from "./context";
 import superjson from "superjson";
 import { classroomRouter } from "./classroom";
 import { userRouter } from "./user";
+import { questionRouter } from "./question";
 
 export const appRouter = createRouter()
   .transformer(superjson)
   .merge('user.', userRouter)
-  .merge("classroom.", classroomRouter);
+  .merge("classroom.", classroomRouter)
+  .merge("question.", questionRouter);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
